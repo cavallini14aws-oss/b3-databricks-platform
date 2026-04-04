dc1a9434a8c11ee5be94046c26674bbb93f22c41
bruno.cavallini <brunocavallini@hotmail.com>
2026-04-04 09:50:19 -0300

    refactor: generate final bundle root and per-environment resources yaml

---
+++ b3_platform/flow_specs/show_databricks_bundle_root.py
@@ -0,0 +1,7 @@
+from b3_platform.flow_specs.generate_databricks_bundle_root import build_bundle_root_yaml
+
+def main() -> None:
+    print(build_bundle_root_yaml())
+
+if __name__ == "__main__":
+    main()
