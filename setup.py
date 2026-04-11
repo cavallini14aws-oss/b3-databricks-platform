6a0987c5bafb20fa8b9434e1af597b61b34519c9
bruno.cavallini <brunocavallini@hotmail.com>
2026-04-11 01:49:37 -0300

    Run ML ops bundle jobs through packaged Python wheel

--- setup.py
+++ setup.py
@@ -8,4 +8,12 @@ setup(
     packages=find_packages(include=["data_platform", "data_platform.*", "pipelines", "pipelines.*"]),
     include_package_data=True,
     install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "run_mlops_drift_cycle=data_platform.mlops.run_mlops_drift_cycle:main",
+            "run_mlops_postprod_cycle=data_platform.mlops.run_mlops_postprod_cycle:main",
+            "run_mlops_retraining_cycle=data_platform.mlops.run_mlops_retraining_cycle:main",
+            "run_mlops_operational_cycle=data_platform.mlops.run_mlops_operational_cycle:main",
+        ]
+    },
 )
